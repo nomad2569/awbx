a5a5605d99f7389af6913fb8065cd4cee4752b17
Kim <alsrb001218@naver.com>
2020-07-11 15:13:39 +0900

    error

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 // Components
-import BGImage from "components/BGImage"
+import BGImage from "../BGImage"
 // Hooks
 import { useHeroQuery } from "hooks/useHeroQuery"
 // Styles
